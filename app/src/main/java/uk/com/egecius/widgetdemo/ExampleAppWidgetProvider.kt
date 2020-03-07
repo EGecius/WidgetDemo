ef84ec68378b0ee97306b7619fcc8c7e7b42b92c
Egidijus Gecius <e.gecius@gmail.com>
2020-03-07 07:36:32 +0000

    ad KDoc to AppWidgetProvider

--- app/src/main/java/uk/com/egecius/widgetdemo/ExampleAppWidgetProvider.kt
+++ app/src/main/java/uk/com/egecius/widgetdemo/ExampleAppWidgetProvider.kt
@@ -2,5 +2,9 @@ package uk.com.egecius.widgetdemo
 
 import android.appwidget.AppWidgetProvider
 
+/**
+ * Defines the basic methods that allow you to programmatically interface with the App Widget, based on broadcast events.
+ * Through it, you will receive broadcasts when the App Widget is updated, enabled, disabled and deleted
+ * */
 class ExampleAppWidgetProvider : AppWidgetProvider() {
 }
