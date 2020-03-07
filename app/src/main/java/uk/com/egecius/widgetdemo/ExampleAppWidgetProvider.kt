908928f530b9f953a6362b039bd4811ab70e4855
Egidijus Gecius <e.gecius@gmail.com>
2020-03-07 10:12:08 +0000

    add KDocs

--- app/src/main/java/uk/com/egecius/widgetdemo/ExampleAppWidgetProvider.kt
+++ app/src/main/java/uk/com/egecius/widgetdemo/ExampleAppWidgetProvider.kt
@@ -3,6 +3,7 @@ package uk.com.egecius.widgetdemo
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.os.Bundle
 import android.util.Log
 
 /**
@@ -11,11 +12,27 @@ import android.util.Log
  * */
 class ExampleAppWidgetProvider : AppWidgetProvider() {
 
+    /**
+     * called to update the App Widget at intervals defined by the updatePeriodMillis attribute in the AppWidgetProviderInfo
+     * */
     override fun onUpdate(
         context: Context?,
         appWidgetManager: AppWidgetManager?,
         appWidgetIds: IntArray?
     ) {
         Log.v("Eg:ExampleAppWidgetProvider:19", "onUpdate() ")
+    }
+
+    /**
+     *  Called when the widget is first placed and any time the widget is resized. You can use this
+     *  callback to show or hide content based on the widget's size ranges. You get the size ranges
+     *  by calling getAppWidgetOptions(), which returns a Bundle */
+    override fun onAppWidgetOptionsChanged(
+        context: Context?,
+        appWidgetManager: AppWidgetManager?,
+        appWidgetId: Int,
+        newOptions: Bundle?
+    ) {
+        Log.v("Eg:ExampleAppWidgetProvider:29", "onAppWidgetOptionsChanged()")
     }
 }
