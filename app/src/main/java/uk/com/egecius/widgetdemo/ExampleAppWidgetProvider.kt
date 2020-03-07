1429756afd6c9da9f592f5f8c5c0a7ac9994b936
Egidijus Gecius <e.gecius@gmail.com>
2020-03-07 10:28:40 +0000

    add a button to open another activity

--- app/src/main/java/uk/com/egecius/widgetdemo/ExampleAppWidgetProvider.kt
+++ app/src/main/java/uk/com/egecius/widgetdemo/ExampleAppWidgetProvider.kt
@@ -1,10 +1,13 @@
 package uk.com.egecius.widgetdemo
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.RemoteViews
 
 /**
  * Defines the basic methods that allow you to programmatically interface with the App Widget, based on broadcast events.
@@ -13,14 +16,41 @@ import android.util.Log
 class ExampleAppWidgetProvider : AppWidgetProvider() {
 
     /**
-     * called to update the App Widget at intervals defined by the updatePeriodMillis attribute in the AppWidgetProviderInfo
+     * Called to update the App Widget at intervals defined by the updatePeriodMillis attribute in the AppWidgetProviderInfo
      * */
-    override fun onUpdate(
-        context: Context?,
-        appWidgetManager: AppWidgetManager?,
-        appWidgetIds: IntArray?
-    ) {
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         Log.v("Eg:ExampleAppWidgetProvider:19", "onUpdate() ")
+
+        setOnClickListeners(appWidgetIds, context, appWidgetManager)
+    }
+
+    private fun setOnClickListeners(
+        appWidgetIds: IntArray,
+        context: Context,
+        appWidgetManager: AppWidgetManager
+    ) {
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        appWidgetIds.forEach { appWidgetId ->
+            val remoteViews: RemoteViews = createLayoutWithListenersSet(context)
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, remoteViews)
+        }
+    }
+
+    private fun createLayoutWithListenersSet(context: Context): RemoteViews {
+        return RemoteViews(
+            context.packageName, R.layout.example_appwidget
+        ).apply {
+            val pendingIntent: PendingIntent = createIntentForExampleActivity(context)
+            setOnClickPendingIntent(R.id.example_button, pendingIntent)
+        }
+    }
+
+    private fun createIntentForExampleActivity(context: Context): PendingIntent {
+        return Intent(context, ExampleActivity::class.java)
+            .let {
+                PendingIntent.getActivity(context, 0, it, 0)
+            }
     }
 
     /**
