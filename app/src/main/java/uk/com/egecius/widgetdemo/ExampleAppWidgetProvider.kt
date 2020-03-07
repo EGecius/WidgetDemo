5f1d91f2495599f7c1a66eb8d2bd251b1145be97
Egidijus Gecius <e.gecius@gmail.com>
2020-03-07 08:09:46 +0000

    add log to onUpdate() method

--- app/src/main/java/uk/com/egecius/widgetdemo/ExampleAppWidgetProvider.kt
+++ app/src/main/java/uk/com/egecius/widgetdemo/ExampleAppWidgetProvider.kt
@@ -1,10 +1,21 @@
 package uk.com.egecius.widgetdemo
 
+import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.util.Log
 
 /**
  * Defines the basic methods that allow you to programmatically interface with the App Widget, based on broadcast events.
  * Through it, you will receive broadcasts when the App Widget is updated, enabled, disabled and deleted
  * */
 class ExampleAppWidgetProvider : AppWidgetProvider() {
+
+    override fun onUpdate(
+        context: Context?,
+        appWidgetManager: AppWidgetManager?,
+        appWidgetIds: IntArray?
+    ) {
+        Log.v("Eg:ExampleAppWidgetProvider:19", "onUpdate() ")
+    }
 }
