4732a380149b2dead9fc7ac01ecb2a16b308a9a8
Egidijus Gecius <e.gecius@gmail.com>
2020-03-09 11:53:15 +0000

    show list of widgets programmatically

--- app/src/main/java/uk/com/egecius/widgetdemo/Constants.kt
+++ app/src/main/java/uk/com/egecius/widgetdemo/Constants.kt
@@ -2,3 +2,4 @@ package uk.com.egecius.widgetdemo
 
 const val KEY_WIDGET_TEXT = "KEY_WIDGET_TEXT"
 const val DEFAULT_PREFS = "DEFAULT_PREFS"
+const val SHOW_WIDGETS_REQUEST_CODE = 8327
--- app/src/main/java/uk/com/egecius/widgetdemo/MainActivity.kt
+++ app/src/main/java/uk/com/egecius/widgetdemo/MainActivity.kt
@@ -1,11 +1,14 @@
 package uk.com.egecius.widgetdemo
 
+import android.appwidget.AppWidgetManager
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +20,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setEditTextListener()
+        setClickListener()
+    }
+
+    private fun setClickListener() {
+        add_widget.setOnClickListener {
+            val pickIntent = Intent(AppWidgetManager.ACTION_APPWIDGET_PICK)
+            // not sure if it's possible to get id of my widget
+            val appWidgetID = "91397"
+            pickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetID)
+            startActivityForResult(pickIntent, SHOW_WIDGETS_REQUEST_CODE)
+        }
     }
 
     private fun setEditTextListener() {
